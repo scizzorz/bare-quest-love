ad3e95f4854f4896e9d44de24dca2f143e84be78
John Weachock <jweachock@gmail.com>
2017-06-12 21:29:08 -0400

    Add basic joystick

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,16 @@
 local Terebi = require('terebi')
 local FPS = 60.0
+local SCALE = 3
+local WIDTH = 160
+local HEIGHT = 240
+
+-------------------------------------------------------------------------------
+
+-- Returns the distance between two points.
+function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
+
+-- Returns the angle between two points.
+function math.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
 
 -------------------------------------------------------------------------------
 
@@ -17,9 +28,14 @@ end
 -------------------------------------------------------------------------------
 
 local framesets = {
-  ui = {
-    size = 8,
-    num = 4,
+  ui_knob = {
+    size = 16,
+    num = 1,
+  },
+
+  ui_socket = {
+    size = 32,
+    num = 1,
   },
 
   actor = {
@@ -64,17 +80,17 @@ local sprites = {
     texture = "map_field",
     frameset = "map",
   },
+
+  ui_knob = {
+    texture = "ui_stick_knob",
+    frameset = "ui_knob",
+  },
+
+  ui_socket = {
+    texture = "ui_stick_socket",
+    frameset = "ui_socket",
+  },
 }
-
-function load_sprite(id)
-  print("loading sprite: " .. id)
-  return {
-    gfx = load_gfx(sprites[id].texture),
-    quads = load_quads(sprites[id].frameset),
-  }
-end
-
--------------------------------------------------------------------------------
 
 local object = {}
 local object_mt = {__index = object}
@@ -85,20 +101,25 @@ function object.new(...)
   return ret
 end
 
-function object:init(sprite_id)
+function object:init(id)
   self.x = 0
   self.y = 0
   self.sx = 1
   self.sy = 1
   self.angle = 0
   self.frame = 0
-  self.sprite = load_sprite(sprite_id)
+  self.visible = true
+
+  self.gfx = load_gfx(sprites[id].texture)
+  self.quads = load_quads(sprites[id].frameset)
 end
 
 function object:draw()
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(self.sprite.gfx, self.sprite.quads[self.frame],
-                     self.x, self.y, self.angle, self.sx, self.sy)
+  if self.visible then
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(self.gfx, self.quads[self.frame],
+                      self.x, self.y, self.angle, self.sx, self.sy)
+  end
 end
 
 -------------------------------------------------------------------------------
@@ -112,25 +133,50 @@ function batch.new(...)
   return ret
 end
 
-function batch:init(sprite_id)
+function batch:init(id, width, height, map_size)
   self.x = 0
   self.y = 0
+  self.visible = true
+
+  self.gfx = load_gfx(sprites[id].texture)
+  self.quads = load_quads(sprites[id].frameset)
+  self.tile_size = framesets[sprites[id].frameset].size
+  self.map_size = map_size
+
   self.map = {}
-  self.sprite = load_sprite(sprite_id)
-  self.batch = love.graphics.newSpriteBatch(self.sprite.gfx, 10 * 15)
+  for x = 0, map_size - 1 do
+    self.map[x] = {}
+    for y = 0, map_size - 1 do
+      self.map[x][y] = (x + y) % 7
+    end
+  end
+
+  self.width = width
+  self.height = height
+  self.batch = love.graphics.newSpriteBatch(self.gfx, width * height)
+
   self:update()
 end
 
 function batch:update()
+  local x_start = math.floor(-self.x / self.tile_size)
+  local y_start = math.floor(-self.y / self.tile_size)
+
   self.batch:clear()
-  self.batch:add(self.sprite.quads[0], 16, 16)
-  self.batch:add(self.sprite.quads[0], 0, 48)
+  for x = 0, self.width - 1 do
+    for y = 0, self.height - 1 do
+      self.batch:add(self.quads[self.map[x_start + x][y_start + y]],
+                     x * self.tile_size, y * self.tile_size)
+    end
+  end
   self.batch:flush()
 end
 
 function batch:draw()
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(self.batch)
+  if self.visible then
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(self.batch)
+  end
 end
 
 -------------------------------------------------------------------------------
@@ -139,11 +185,25 @@ function love.load()
   print("love.load")
   Terebi.initializeLoveDefaults()
 
-  screen = Terebi.newScreen(160, 240, 3)
+  screen = Terebi.newScreen(WIDTH, HEIGHT, SCALE)
   bare = object.new("bare")
-  map = batch.new("map_field")
+  map = batch.new("map_field", 12, 17, 20)
+  knob = object.new("ui_knob")
+  socket = object.new("ui_socket")
+  knob.visible = false
+  socket.visible = false
+
+  pressed = false
+  stick_x_start = 0.0
+  stick_y_start = 0.0
+  stick_x_cur = 0.0
+  stick_y_cur = 0.0
+  stick_x = 0.0
+  stick_y = 0.0
   elapsed = 0.0
 end
+
+-------------------------------------------------------------------------------
 
 function love.update(dt)
   elapsed = elapsed + dt
@@ -168,6 +228,8 @@ function love.tick()
   ]]
 end
 
+-------------------------------------------------------------------------------
+
 function love.draw()
   love.graphics.setCanvas(screen:getCanvas())
 
@@ -176,6 +238,53 @@ function love.draw()
 
   map:draw()
   bare:draw()
+  socket:draw()
+  knob:draw()
 
   screen:draw()
+end
+
+-------------------------------------------------------------------------------
+
+function love.mousepressed(x, y)
+  pressed = true
+  socket.visible = true
+  knob.visible = true
+
+  stick_x_start = (x/SCALE)
+  stick_y_start = (y/SCALE)
+
+  socket.x = stick_x_start - 16
+  socket.y = stick_y_start - 16
+
+  stick_x_cur = (x/SCALE)
+  stick_y_cur = (y/SCALE)
+
+  knob.x = stick_x_cur - 8
+  knob.y = stick_y_cur - 8
+end
+
+function love.mousereleased(x, y)
+  pressed = false
+  socket.visible = false
+  knob.visible = false
+end
+
+function love.mousemoved(x, y, dx, dy)
+  if pressed then
+    stick_x_cur = (x/SCALE)
+    stick_y_cur = (y/SCALE)
+
+    local angle = math.angle(stick_x_start, stick_y_start, stick_x_cur, stick_y_cur)
+    local dist = math.dist(stick_x_start, stick_y_start, stick_x_cur, stick_y_cur)
+    if dist > 8 then
+      dist = 8
+    end
+
+    stick_x_cur = stick_x_start + math.cos(angle) * dist
+    stick_y_cur = stick_y_start + math.sin(angle) * dist
+
+    knob.x = stick_x_cur - 8
+    knob.y = stick_y_cur - 8
+  end
 end
