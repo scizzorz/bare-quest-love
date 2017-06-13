e88c60d0ecd1e9a3be2a23fbca625227b43b623e
John Weachock <jweachock@gmail.com>
2017-06-12 20:15:10 -0400

    Add basic "object" idea

--- src/main.lua
+++ src/main.lua
@@ -1,61 +1,121 @@
 local Terebi = require('terebi')
+local FPS = 60.0
 
-gfx = {
-  actor_bare = {
-    fsize = 16,
-    fnum = 4,
-  }
-}
+-------------------------------------------------------------------------------
 
-text = {
-  img = nil,
-  x = 0,
-  dx = 1,
-  min_x = 0,
-  max_x = 144,
-
-  y = 0,
-  dy = 1,
-  min_y = 0,
-  max_y = 224,
-}
+local gfx = {}
 
 function load_gfx(id)
-  if gfx[id].img == nil then
-    gfx[id].img = love.graphics.newImage("gfx/" .. id .. ".png")
-    gfx[id].frames = {}
-
-    local fnum = gfx[id].fnum
-    local fsize = gfx[id].fsize
-    for x = 0, fnum - 1 do
-      for y = 0, fnum - 1 do
-        gfx[id].frames[y*fnum + x] = love.graphics.newQuad(fsize * x, fsize * y, fsize, fsize, fsize*fnum, fsize*fnum)
-      end
-    end
+  if gfx[id] == nil then
+    print("loading gfx: " .. id)
+    gfx[id] = love.graphics.newImage("gfx/" .. id .. ".png")
   end
 
   return gfx[id]
 end
 
+-------------------------------------------------------------------------------
+
+local framesets = {
+  ui = {
+    size = 8,
+    num = 4,
+  },
+
+  actor = {
+    size = 16,
+    num = 4,
+  },
+
+  map = {
+    size = 16,
+    num = 8,
+  },
+}
+
+local quads = {}
+
+function load_quads(id)
+  if quads[id] == nil then
+    print("loading quads: " .. id)
+    quads[id] = {}
+
+    local num = framesets[id].num
+    local size = framesets[id].size
+    for x = 0, num - 1 do
+      for y = 0, num - 1 do
+        quads[id][y*num + x] = love.graphics.newQuad(size * x, size * y, size, size, size*num, size*num)
+      end
+    end
+  end
+
+  return quads[id]
+end
+
+-------------------------------------------------------------------------------
+
+local sprites = {
+  bare = {
+    texture = "actor_bare",
+    frameset = "actor",
+  },
+}
+
+function load_sprite(id)
+  print("loading sprite: " .. id)
+  return {
+    gfx = load_gfx(sprites[id].texture),
+    quads = load_quads(sprites[id].frameset),
+  }
+end
+
+-------------------------------------------------------------------------------
+
+local object = {}
+local object_mt = {__index = object}
+
+function object.new(...)
+  return setmetatable({}, object_mt):init(...)
+end
+
+function object:init(sprite_id)
+  self.x = 0
+  self.y = 0
+  self.sx = 1
+  self.sy = 1
+  self.angle = 0
+  self.frame = 0
+  self.sprite = load_sprite(sprite_id)
+  return self
+end
+
+function object:draw()
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(self.sprite.gfx, self.sprite.quads[self.frame],
+                     self.x, self.y, self.angle, self.sx, self.sy)
+end
+
+-------------------------------------------------------------------------------
 
 function love.load()
+  print("love.load")
   Terebi.initializeLoveDefaults()
 
   screen = Terebi.newScreen(160, 240, 3)
-  text.gfx = load_gfx("actor_bare")
+  bare = object.new("bare")
   elapsed = 0.0
 end
 
-
 function love.update(dt)
   elapsed = elapsed + dt
-  if elapsed > 1.0 / 6.0 then
+  if elapsed > 1.0 / FPS then
     love.tick()
     elapsed = 0.0
   end
 end
 
 function love.tick()
+  --[[
   text.x = text.x + text.dx
   text.y = text.y + text.dy
   if text.x < text.min_x or text.x > text.max_x then
@@ -65,16 +125,15 @@ function love.tick()
   if text.y < text.min_y or text.y > text.max_y then
     text.dy = text.dy * -1
   end
+  ]]
 end
-
 
 function love.draw()
   love.graphics.setCanvas(screen:getCanvas())
 
-  love.graphics.setColor(50, 0, 0)
+  love.graphics.setColor(0, 0, 0)
   love.graphics.rectangle('fill', 0, 0, 160, 240)
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(text.gfx.img, text.gfx.frames[0], text.x, text.y)
+  bare:draw()
 
   screen:draw()
 end
